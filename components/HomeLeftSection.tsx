7d0c9ea1076573beed5201572a9023a3b9cc72ad
JC Castaneda <jcing.jc@gmail.com>
2024-09-06 02:58:56 +0800

    ota

--- components/HomeLeftSection.tsx
+++ components/HomeLeftSection.tsx
@@ -35,7 +35,7 @@ export default function HomeLeftSection() {
           }}
         >
           <ThemedText type="title" style={{ color: "white" }}>
-            Synctimes - Over-the-Air (99)
+            Synctimes - Over-the-Air (100)
           </ThemedText>
           <ThemedTouchableOpacity
             onPress={signOut}
