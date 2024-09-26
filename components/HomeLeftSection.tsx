0ee7016abe29730ba6b6f896258767411d555b07
JC Castaneda <jcing.jc@gmail.com>
2024-09-26 18:31:47 +0800

    105

--- components/HomeLeftSection.tsx
+++ components/HomeLeftSection.tsx
@@ -35,7 +35,7 @@ export default function HomeLeftSection() {
           }}
         >
           <ThemedText type="title" style={{ color: "white" }}>
-            Synctimes - Over-the-Air-Updates (104)
+            Synctimes - Over-the-Air-Updates (105)
           </ThemedText>
           <ThemedTouchableOpacity
             onPress={signOut}
